adfcd742ee914abee84d806799702cc04a48fc07
Hammad-Zubair-Off <hammadzubairofficial@gmail.com>
2025-05-18 10:57:37 +0500

    update cors issues

--- server.js
+++ server.js
@@ -15,9 +15,8 @@ app.use(express.json());
 // CORS configuration
 app.use(cors({
   origin: [
-    'https://ilm-hub-an-online-exam-platform.vercel.app',
     'http://localhost:3000',
-    'https://temp-red-gamma.vercel.app/'  // Add your deployed client URL here
+    'https://temp-red-gamma.vercel.app'  // Frontend URL
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
