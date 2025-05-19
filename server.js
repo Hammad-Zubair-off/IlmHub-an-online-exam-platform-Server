05c150fd921e3eb27b010ba862f57db418d1fc55
Hammad-Zubair-Off <hammadzubairofficial@gmail.com>
2025-05-20 02:35:04 +0500

    Update the frontend Cors error

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cors({
   origin: [
     // 'https://ilm-hub-an-online-exam-platform.vercel.app',
     'http://localhost:3000',
-    'https://temp-red-gamma.vercel.app/'  // Add your deployed client URL here
+    'https://ilm-hub-an-online-exam-platform-client1-o.vercel.app/'  // Add your deployed client URL here
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
